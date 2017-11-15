4dc5db58cb88cb57d57b9fa0ab5184fd9aedaede
Your Name <you@example.com>
2017-11-16 12:52:25 +1300

    Render id, logo, photo

--- src/components/Property.test.tsx
+++ src/components/Property.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {shallow} from 'enzyme';
-import {Property} from './Property';
+import { shallow } from 'enzyme';
+import { Property } from './Property';
 
 describe('Property', () => {
   const props = {
@@ -19,5 +19,29 @@ describe('Property', () => {
     expect(shallow(
       <Property {...props} />
     ).length).toEqual(1);
+  });
+
+  it('Renders id', () => {
+    const wrapper = shallow(
+      <Property {...props} />
+    );
+
+    expect(wrapper.find('.property__id').length).toEqual(1);
+  });
+
+  it('Renders logo', () => {
+    const wrapper = shallow(
+      <Property {...props} />
+    );
+
+    expect(wrapper.find('.property__logo').length).toEqual(1);
+  });
+
+  it('Renders photo', () => {
+    const wrapper = shallow(
+      <Property {...props} />
+    );
+
+    expect(wrapper.find('.property__photo').length).toEqual(1);
   });
 });
--- src/components/Property.tsx
+++ src/components/Property.tsx
@@ -9,7 +9,9 @@ export const Property = (
     <div
       className="property"
     >
-      {property.id}
+      <p className="property__id">ID: {property.id}</p>
+      <p className="property__logo"><img src={property.agency.logo}/></p>
+      <p className="property__photo"><img src={property.mainImage} width={400}/></p>
     </div>
   );
 };
