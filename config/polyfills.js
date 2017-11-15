565b2164e4885a9353831b2aa843cc6b31c08678
Your Name <you@example.com>
2017-11-16 12:40:11 +1300

    Fix console error about requestAnimationFrame when running tests

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// Resolution for requestAnimationFrame not supported in jest error :
+// https://github.com/facebook/react/issues/9102#issuecomment-283873039
+global.window = global;
+window.addEventListener = () => {};
+window.requestAnimationFrame = () => {
+  throw new Error('requestAnimationFrame is not supported in Node');
+};
+
 var enzyme = require('enzyme');
 var Adapter = require('enzyme-adapter-react-16');
 
