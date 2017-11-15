b5ce1d65c4d23737bfe15946d08a9d56fd0889b0
Your Name <you@example.com>
2017-11-15 23:44:01 +1300

    Remove unnecessary code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,14 +7,6 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 });
 
-// Display the list of properties using the data set within the "results" array running down the page in a column that has a heading labeled Results.
-// There will be a second column with a heading "Saved Properties", along side the "Results" column.
-// It will contain the initial property within the "saved" array.
-// Hovering over a property card in the "Results" column will display an "add" button.
-// Clicking the "add" button will create the property in the "Saved".
-// Properties column. Hovering over a property card in the "Saved Properties" column will display a "remove" button.
-// Clicking the "remove" button will remove the property from the list of saved properties.
-
 it(
   `Display the list of properties
   using the dataset within the "Results" array
